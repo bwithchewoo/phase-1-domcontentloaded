999bba93bfea6a1c0686663e14520a0e3e054251
Anna Cho <loveablessing@gmail.com>
2022-06-11 11:41:44 -0400

    commit tree 19672d73fb99964428815304247bd045d48ca2ef

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('Event was triggered')
+    //document.getElementById('text').innerHTML = document.getElementById('text').innerHTML.replace('JavaScript is so cool. It lets me add text to my page programmatically.', 'This is really cool!')
+    let elementText = document.getElementById('text');
+    elementText.innerHTML = elementText.innerHTML.replace('JavaScript is so cool. It lets me add text to my page programmatically.', 'This is really cool!')
+    console.log(elementText)
+});
+  
